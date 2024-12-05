7721767c16bbe62a1ebd01313e7b428f8481b593
intxaurtietadev <t.intxaurtietadev@gmail.com>
2024-12-05 08:54:51 +0100

    arguments

--- js/rest.js
+++ js/rest.js
@@ -1,6 +1,15 @@
+//REST
+
 function duplicar(...numeros) {
     numeros.forEach(numero => console.log(numero * 2));
   }
   
   duplicar(1, 2, 3, 4); 
   
+// ARGUMENTS
+
+function duplicarAR() {
+  Array.from(arguments).forEach(numero => console.log(numero * 2));
+}
+
+duplicarAR(1, 2, 3, 4);
