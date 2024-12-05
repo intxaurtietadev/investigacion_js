84a30d5b394e71ba248b7cdd26a502d30399b681
intxaurtietadev <t.intxaurtietadev@gmail.com>
2024-12-05 12:06:27 +0100

    arguments

--- js/flecha.js
+++ js/flecha.js
@@ -1,13 +1,13 @@
+
 const sumaArr = (a, b) => {
     return a + b;
-}
-console.log(sumar (2, 3));
+};
+console.log(sumaArr(2, 3)); // Resultado: 5
 
 
 const suma = (a, b) => a + b;
-
-console.log (suma(2, 3));
+console.log(suma(2, 3)); // Resultado: 5
 
 
 document.write('<p class="red-text">Abre la consola!!</p>');
-document.write('<p class ="blue-text">Funciones flecha🔝🔝🔝</p>');
+document.write('<p class="blue-text">Funciones flecha🔝🔝🔝</p>');
